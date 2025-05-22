b54444dd7149b4323ffe7cf238b43337c0212672
Etheriouse <liayel@Zera.>
2025-05-23 00:11:48 +0200

    tabs

---
+++ src/renderer.js
@@ -0,0 +1,7 @@
+import * as tabs from './tabs.js'
+
+tabs._add_tab("Yuzu Compiler");
+tabs._add_tab("Clipboard");
+tabs._add_tab("Dashboard");
+tabs._add_tab("White board");
+tabs._add_tab("Artorias");
---
+++ src/tabs.js
@@ -0,0 +1,41 @@
+const tabs = [];
+const deleted_tab = []; // max 10;
+
+document.getElementById('add-tab').addEventListener('click', () => {
+    _add_tab("Clipboard");
+})
+
+function _sub_tab(element) {
+    const tab_bar = document.getElementById('tab-bar');
+    tab_bar.removeChild(element);
+}
+
+export function _add_tab(type) {
+    const tab_bar = document.getElementById('tab-bar');
+
+    const tab = document.createElement('div');
+    const div = document.createElement('div');
+    const icon = document.createElement('img');
+    const x = document.createElement('img');
+    const title = document.createElement('h3');
+
+    tab.className = 'tab';
+    icon.className = 'icon';
+    icon.src = "../assets/icons/tools/Clipboard.png";
+    x.className = 'x';
+    x.src = "../assets/icons/x.png";
+
+    x.addEventListener('click', () => {
+        _sub_tab(tab);
+    })
+
+    title.innerHTML = type;
+
+    div.appendChild(icon);
+    div.appendChild(title);
+
+    tab.appendChild(div);
+    tab.appendChild(x);
+
+    tab_bar.insertBefore(tab, document.getElementById('add-tab'));
+}
